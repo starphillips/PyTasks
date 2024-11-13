fccbc60c19f3d306e7a14a427ae2236001ce00cc
Starkie Phillips <star.phillips03@gmail.com>
2024-11-13 11:52:13 +0000

    change from index to app css and js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,7 +14,31 @@ function App() {
   );
 }
 
+// Get modal element
+var modal = document.getElementById("createTask");
 
+// Get button that opens the popup
+var btn = document.getElementById("openPopUp");
+
+// Get the <span> element that closes the popup
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the popup
+btn.onclick = function () {
+  modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside the popup, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
